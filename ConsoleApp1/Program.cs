b97da90065003daec70fed0eb50f89391316d28b
Davidson Gonçalves <davidson.cg7@gmail.com>
2020-08-24 22:13:03 -0300

    Acrescentado a pergunta como você esta

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
-            
+            Console.WriteLine("Como voce esta hoje?");
+
         }
     }
 }
